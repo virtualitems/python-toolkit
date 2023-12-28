cb13c8c2fcb6b9ccfaa05f10cd501fe2eb6dd5e1
acarrasco <acarrasco@deltec.com.co>
2023-12-28 12:28:08 -0500

    added architecture directory

---
+++ architecture/hexagonal/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+docstring
+"""
+
+# standard
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import logging
+
+# framework
+
+# third-party
+
+# first-party
+
+# typing
+if TYPE_CHECKING:  # pragma: no cover
+    pass
